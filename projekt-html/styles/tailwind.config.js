936fb7c8de5584dde2368ec4fbe7d34353a2f735
hubert-kniola <hubert.kniola@gmail.com>
2022-03-12 13:14:21 +0100

    init

---
+++ projekt-html/styles/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    // ...
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // ...
+  ],
+}
