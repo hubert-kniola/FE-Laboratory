588fb52479bf3b0750c8855b1549b894558ebf61
Hubert <hubert.kniola@student.put.poznan.pl>
2022-03-25 00:15:34 +0100

    html skelet

---
+++ projekt-html/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
