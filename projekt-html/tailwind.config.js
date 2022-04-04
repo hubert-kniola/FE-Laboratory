db0a4f439f093817f2cb31baa138616885022935
Hubert <hubert.kniola@student.put.poznan.pl>
2022-04-04 23:01:16 +0200

    prefin

--- projekt-html/tailwind.config.js
+++ projekt-html/tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        logoBlue: {
+          500: "#009de2",
+        },
+      },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
