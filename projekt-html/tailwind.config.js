1a51ce0df6f4ee51331613529d73db5e780e27a6
Hubert <hubert.kniola@student.put.poznan.pl>
2022-04-05 23:19:58 +0200

    x

--- projekt-html/tailwind.config.js
+++ projekt-html/tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        plane1: "url('./images/plane1.jpg')",
+        plane2: "url('./images/plane2.jpg')",
+        plane3: "url('./images/plane3.jpg')",
+        plane4: "url('./images/plane4.jpg')",
+        plane5: "url('./images/plane5.jpg')",
+      }),
       colors: {
         logoBlue: {
           500: "#009de2",
