68a13a117ef1fefb7cce2039d53a26251d06384e
Hubert <hubert.kniola@student.put.poznan.pl>
2022-05-16 00:13:08 +0200

    svelte styles

--- projekt-svelte/src/lib/tabs/Tabs.svelte
+++ projekt-svelte/src/lib/tabs/Tabs.svelte
@@ -1,40 +1,59 @@
 <script>
-	import { createEventDispatcher } from 'svelte';
-	let dispatch = createEventDispatcher();
-	
-	export let items;
-	export let activeItem;
-  </script>
-  
-  <div class="tabs">
-	<ul>
-	  {#each items as item}
-		<li on:click={() => dispatch('tabChange', item)}>
-		  <div class:active={item === activeItem}>{ item }</div>
-		</li>
-	  {/each}
-	</ul>
-  </div>
-  
-  <style>
-	.tabs{
-	  margin-bottom: 40px;
-	}
-	ul{
-	  display: flex;
-	  justify-content: center;
-	  padding: 0;
-	  list-style-type: none;
-	}
-	li{
-	  margin: 0 16px;
-	  font-size: 18px;
-	  color: #555;
-	  cursor: pointer;
-	}
-	.active{
-	  color: #d91b42;
-	  border-bottom: 2px solid #d91b42;
-	  padding-bottom: 8px;
-	}
-  </style>
+  import { createEventDispatcher } from "svelte";
+  let dispatch = createEventDispatcher();
+
+  export let items;
+  export let activeItem;
+</script>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <li
+        class:active={item === activeItem}
+        on:click={() => dispatch("tabChange", item)}
+      >
+        {item}
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .tabs {
+    padding: 1rem;
+    border: solid 2px #d91b42;
+    border-radius: 12px;
+    box-shadow: #d91b42 0px 6px 12px -2px, #d91b42 0px 3px 7px -3px;
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1rem;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+  }
+  li {
+    font-size: 16px;
+    color: #555;
+    cursor: pointer;
+    border: solid 2px #d91b42;
+    border-radius: 12px;
+    box-shadow: #545454 0px 4px 6px -2px, #545454 0px 3px 5px -3px;
+    padding: 0.5rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+
+  li:hover {
+	box-shadow: #d91b42 0px 6px 12px -2px, #d91b42 0px 3px 7px -3px;
+  }
+
+  .active {
+    color: #d91b42;
+	box-shadow: #d91b42 0px 4px 6px -2px, #d91b42 0px 3px 5px -3px;
+  }
+</style>
--- projekt-svelte/src/routes/index.svelte
+++ projekt-svelte/src/routes/index.svelte
@@ -5,17 +5,26 @@
   let items = ["All", "ToDo", "Done"];
   let activeItem = "All";
   const tabChange = (e) => (activeItem = e.detail);
+
+  const handleClick = () => {};
 </script>
 
 <main>
-  <Tabs {activeItem} {items} on:tabChange={tabChange} />
-  {#if activeItem === "All"}
-    <p>poll list goes here</p>
-  {:else if activeItem === "ToDo"}
-    <p>poll list goes here</p>
-  {:else if activeItem === "Done"}
-    <p>poll list goes here</p>
-  {/if}
+  <div class="app">
+    <Tabs {activeItem} {items} on:tabChange={tabChange} />
+    {#if activeItem === "All"}
+      <p>poll list goes here 1</p>
+    {:else if activeItem === "ToDo"}
+      <p>poll list goes here 2</p>
+    {:else if activeItem === "Done"}
+      <p>poll list goes here 3</p>
+    {/if}
+
+    <div class="button--container">
+      <input class="button--input" type="text" />
+      <button class="button" on:click|once={handleClick}>CREATE</button>
+    </div>
+  </div>
 </main>
 
 <style>
@@ -23,5 +32,38 @@
     width: 100%;
     max-width: 960px;
     margin: 40px auto;
+  }
+
+  p {
+    color: white;
+  }
+
+  .app {
+    border: solid 2px #d91b42;
+    border-radius: 12px;
+    box-shadow: #d91b42 0px 6px 12px -2px, #d91b42 0px 3px 7px -3px;
+  }
+
+  .button--container {
+    flex-direction: row;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .button--input {
+    background-color: transparent;
+    border-radius: 12px;
+    outline: none;
+    border: solid 2px white;
+    color: #d91b42;
+    padding: 0.3rem;
+  }
+
+  .button {
+    color: #d91b42;
+  }
+  
+  .button:hover {
+    color: white;
   }
 </style>
